e0ada1fc55e0c11c0b9a573acd4e7fc7fa10f8bc
Heike Winkelvoß <public@egladil.de>
2022-04-01 17:14:34 +0200

    workspace and main app initialized

---
+++ frontend/mathe-jung-alt-workspace/apps/mja-aufgabenarchiv-admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mathe-jung-alt-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'mja-aufgabenarchiv-admin';
+}
