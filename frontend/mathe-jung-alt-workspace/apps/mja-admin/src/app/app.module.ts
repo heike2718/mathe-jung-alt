ac689b196d62d4299aed8c5d74443a11f6e07594
Heike Winkelvoß <public@egladil.de>
2022-04-02 09:46:04 +0200

    ngrx added

--- frontend/mathe-jung-alt-workspace/apps/mja-admin/src/app/app.module.ts
+++ frontend/mathe-jung-alt-workspace/apps/mja-admin/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './+state';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot(reducers, { metaReducers }), !environment.production ? StoreDevtoolsModule.instrument() : []],
   providers: [],
   bootstrap: [AppComponent],
 })
