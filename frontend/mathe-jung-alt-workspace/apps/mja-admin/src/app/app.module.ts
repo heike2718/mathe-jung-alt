f27ba9f90bdbec29253d6c34b331644a61357807
Heike Winkelvoß <public@egladil.de>
2022-04-03 08:54:40 +0200

    material added

--- frontend/mathe-jung-alt-workspace/apps/mja-admin/src/app/app.module.ts
+++ frontend/mathe-jung-alt-workspace/apps/mja-admin/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { RaetselDomainModule } from '@mathe-jung-alt-workspace/raetsel/domain';
 import { RaetselFeatureSearchModule } from '@mathe-jung-alt-workspace/raetsel/feature-search';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,6 +20,7 @@ import { RaetselFeatureSearchModule } from '@mathe-jung-alt-workspace/raetsel/fe
     EffectsModule.forRoot([]),
     StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
