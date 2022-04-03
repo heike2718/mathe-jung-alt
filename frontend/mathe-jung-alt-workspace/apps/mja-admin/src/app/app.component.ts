88aa54bc75f16e1fd047b1056c128365c389980e
Heike Winkelvoß <public@egladil.de>
2022-04-03 08:15:47 +0200

    migration from css to scss

--- frontend/mathe-jung-alt-workspace/apps/mja-admin/src/app/app.component.ts
+++ frontend/mathe-jung-alt-workspace/apps/mja-admin/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mja-admin-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'mja-admin';
