216052ae2da74cb80bff08311f090a07be75f439
Heike Winkelvoß <public@egladil.de>
2022-07-03 18:25:42 +0200

    lazy loading module tut immer noch nicht

---
+++ frontend/mja-workspace/apps/mja-admin/src/app/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthFacade } from '@mja-workspace/shared/auth/domain';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'mja-admin-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  
+  titel = 'Mja Admin (' + environment.version + ')';
+
+  constructor(public authFacade: AuthFacade) { }
+
+  ngOnInit(): void { }
+
+  public login(): void {
+    this.authFacade.requestLoginRedirectUrl();
+  }
+
+  public logout(): void {
+    this.authFacade.logout();
+  }
+  
+}
