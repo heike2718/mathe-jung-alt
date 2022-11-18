c1be522043333be736eab28cb2eba5299b66de2b
Heike Winkelvoß <public@egladil.de>
2022-11-18 18:46:40 +0100

    nrwl-template with angular and ngrx

---
+++ frontend/mja-ws/apps/mja-admin/src/main.ts
@@ -0,0 +1,42 @@
+import { enableProdMode, importProvidersFrom, LOCALE_ID } from '@angular/core';
+
+import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+
+import localeDe from '@angular/common/locales/de';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+registerLocaleData(localeDe, 'de');
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(),
+
+    provideStore(),
+    provideEffects([]),
+    provideStoreDevtools(),
+
+    {
+      provide: MAT_DATE_LOCALE,
+      useValue: 'de',
+    },    
+    { provide: LOCALE_ID, useValue: 'de' },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
+  ],
+});
