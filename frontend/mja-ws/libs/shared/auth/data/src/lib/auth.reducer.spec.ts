a14be98267414a404e4166f6911a444b3ee59caf
Heike Winkelvoß <public@egladil.de>
2022-11-26 14:22:49 +0100

    jest endlich zum Laufen gebracht

--- frontend/mja-ws/libs/shared/auth/data/src/lib/auth.reducer.spec.ts
+++ frontend/mja-ws/libs/shared/auth/data/src/lib/auth.reducer.spec.ts
@@ -1,13 +1,13 @@
-import { Store } from '@ngrx/store';
-import {authFeature, initialState} from './auth.reducer';
+import "@angular/compiler";  // erforderlich weil man aktuell eine bescheuerte Fehlermeldung von jest bekommt, obwohl alles korrekt konfigiriert ist
+import { authFeature, initialState } from './auth.reducer';
 import { authActions } from './auth.actions';
-import { Session } from './internal.model';
+import { anonymousSession, Session } from './internal.model';
 
 describe('Auth Reducer', () => {
 
-    it ('foo', () => {
+    it('should replace the session in state when session created', () => {
 
-        const session: Session = {
+        const theNewSession: Session = {
             expiresAt: 99999,
             user: {
                 anonym: false,
@@ -17,8 +17,23 @@ describe('Auth Reducer', () => {
             }
         }
         const state = initialState;
-        const newState = authFeature.reducer(state, authActions.sessionCreated({session: session}));
-        expect(state).toBe(newState);
+        const newState = authFeature.reducer(state, authActions.sessionCreated({ session: theNewSession }));
+        expect(newState.session).toBe(theNewSession);
+    });
 
+    it('should reset the session when logged out', () => {
+
+        const theSession: Session = {
+            expiresAt: 99999,
+            user: {
+                anonym: false,
+                fullName: 'Ponder Stibbons',
+                idReference: 'hjkahkj',
+                roles: ['ADMIN', 'STANDARD']
+            }
+        }
+        const state = {...initialState, session: theSession};
+        const newState = authFeature.reducer(state, authActions.loggedOut());
+        expect(newState.session).toBe(anonymousSession);
     });
 });
