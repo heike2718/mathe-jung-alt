7de5a3d31f3da78a89f8a788bd50266e9dfff1bc
Heike Winkelvoß <public@egladil.de>
2023-09-06 18:53:40 +0200

    I0083: zeug gelöscht

--- backend/latex-client/src/main/bin/call-docker-pdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# ruft den docker-latex-container mit dem skript latex2pdf auf
-
-docker container exec latex /bin/latex2pdf.sh $1
--- backend/latex-client/src/main/bin/call-docker-png.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# ruft den docker-latex-container mit dem skript latex2png auf
-
-docker container exec latex /bin/latex2png.sh $1
