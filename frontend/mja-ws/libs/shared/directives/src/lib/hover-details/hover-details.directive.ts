99f2eeee48a7870f45fc4dee7d02457f9950bf41
Heike Winkelvoß <public@egladil.de>
2024-01-21 15:07:33 +0100

    I0131: details-tooltip weiß

--- frontend/mja-ws/libs/shared/directives/src/lib/hover-details/hover-details.directive.ts
+++ frontend/mja-ws/libs/shared/directives/src/lib/hover-details/hover-details.directive.ts
@@ -4,7 +4,7 @@ import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/
   selector: '[mjaHoverDetails]',
   standalone: true,
 })
-export class HoverDetailsDirective {  
+export class HoverDetailsDirective {
 
   @Input()
   mjaHoverDetails!: string;
@@ -20,20 +20,24 @@ export class HoverDetailsDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   #showDetails(event: MouseEvent) {
-    // Use Renderer2 to manipulate the DOM
-    const detailsElement = this.renderer.createElement('div');
-    this.renderer.addClass(detailsElement, 'hover-details');
-    const text = this.renderer.createText(this.mjaHoverDetails);
-    this.renderer.appendChild(detailsElement, text);
-    this.renderer.appendChild(this.el.nativeElement, detailsElement);
 
-    // Position the tooltip near the cursor
-    const offsetX = 10; // Adjust this value to control the distance from the cursor
-    const offsetY = 10; // Adjust this value to control the distance from the cursor
 
-    this.renderer.setStyle(detailsElement, 'position', 'fixed');
-    this.renderer.setStyle(detailsElement, 'top', `${event.clientY + offsetY}px`);
-    this.renderer.setStyle(detailsElement, 'left', `${event.clientX + offsetX}px`);
+    if (this.mjaHoverDetails && this.mjaHoverDetails.trim().length > 0) {
+      // Use Renderer2 to manipulate the DOM
+      const detailsElement = this.renderer.createElement('div');
+      this.renderer.addClass(detailsElement, 'hover-details');
+      const text = this.renderer.createText(this.mjaHoverDetails);
+      this.renderer.appendChild(detailsElement, text);
+      this.renderer.appendChild(this.el.nativeElement, detailsElement);
+
+      // Position the tooltip near the cursor
+      const offsetX = 10; // Adjust this value to control the distance from the cursor
+      const offsetY = 10; // Adjust this value to control the distance from the cursor
+
+      this.renderer.setStyle(detailsElement, 'position', 'fixed');
+      this.renderer.setStyle(detailsElement, 'top', `${event.clientY + offsetY}px`);
+      this.renderer.setStyle(detailsElement, 'left', `${event.clientX + offsetX}px`);
+    }
   }
 
   #hideDetails() {
